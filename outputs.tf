f0217a4afcf7931a5aa23de09ddba8069b0d1405
Kyle Ruddy <kmruddy@users.noreply.github.com>
2021-04-15 18:56:40 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "elb_name" {
   description = "ELB Name"
-  value       = aws_elb.name
+  value       = aws_elb.web.name
 }
 
 output "elb_sg" {
   description = "ELB Security Group Name"
-  value       = aws_elb.source_security_group
+  value       = aws_elb.web.source_security_group
 }
